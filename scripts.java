fbf0840e8271d4e572bfa0d5b22e9d7e06f6064c
LukaCosicWEb <nutz37921@gmail.com>
2025-04-06 21:26:01 +0200

    Delete scripts.java

--- scripts.java
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleButton = document.querySelector('#toggleContent');
-    const content = document.querySelector('#hiddenContent');
-    
-    toggleButton.addEventListener('click', function() {
-        content.classList.toggle('hidden');
-    });
-});
